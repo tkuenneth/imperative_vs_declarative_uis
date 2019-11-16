82b667ea3ff8e2484cae7e74d7ea70a0f081fef4
Thomas Kuenneth <kuennetht@gmail.com>
2019-11-16 15:17:05 +0100

    SwiftUI demo added

---
+++ SwiftUI/CounterDemoSwiftUI/CounterDemoSwiftUI/ContentView.swift
@@ -0,0 +1,32 @@
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State
+    var counter: Int = 0
+    
+    var body: some View {
+        VStack(alignment: .center) {
+            VStack(alignment: .center) {
+                if (counter == 0) {
+                    Text("Noch nicht geklickt")
+                        .font(Font.system(size: 14))
+                        .italic()
+                }
+                else {
+                    Text("\(counter)")
+                        .font(Font.system(size: 72))
+                        .bold()
+                }
+            }
+            .frame(height: 100)
+            .fixedSize()
+            Button(action: {
+                self.counter += 1
+            })
+            {
+                Text("Klick")
+            }
+        }
+    }
+}
