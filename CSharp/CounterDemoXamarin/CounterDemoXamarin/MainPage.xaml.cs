41375432200c8104f0e05c3176c0ce03418b52a8
Thomas Kuenneth <kuennetht@gmail.com>
2019-11-27 12:14:20 +0100

    CounterDemoXamarin added

---
+++ CSharp/CounterDemoXamarin/CounterDemoXamarin/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace CounterDemoXamarin
+{
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public ViewModel ViewModel { get; } = new ViewModel();
+
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            if (ViewModel.Count == 0)
+            {
+                ViewModel.LabelFontSize = 72.0;
+                ViewModel.LabelFontAttributes = FontAttributes.Bold;
+            }
+            ViewModel.Label = $"{++ViewModel.Count}";
+        }
+    }
+}
---
+++ CSharp/CounterDemoXamarin/CounterDemoXamarin/ViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Xamarin.Forms;
+
+namespace CounterDemoXamarin
+{
+    public class ViewModel : ViewModelBase
+    {
+        private FontAttributes _labelFontAttributes = FontAttributes.Italic;
+        public FontAttributes LabelFontAttributes
+        {
+            get => _labelFontAttributes;
+            set => SetProperty(ref _labelFontAttributes, value, nameof(LabelFontAttributes));
+        }
+
+        private double _labelFontSize = 14.0;
+        public double LabelFontSize
+        {
+            get => _labelFontSize;
+            set => SetProperty(ref _labelFontSize, value, nameof(LabelFontSize));
+        }
+
+        private string _label = "Noch nicht geklickt";
+        public string Label
+        {
+            get => _label;
+            set => SetProperty(ref _label, value, nameof(Label));
+        }
+
+        public int Count { get; set; }
+    }
+}
