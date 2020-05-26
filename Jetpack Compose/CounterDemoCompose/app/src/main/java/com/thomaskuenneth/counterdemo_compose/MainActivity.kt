35c405666ff6b3806792086f1e232a6c34851676
Thomas Kuenneth <kuennetht@gmail.com>
2020-05-26 14:46:39 +0200

    Example works again with latest Jetpack Compose version

--- Jetpack Compose/CounterDemoCompose/app/src/main/java/com/thomaskuenneth/counterdemo_compose/MainActivity.kt
+++ Jetpack Compose/CounterDemoCompose/app/src/main/java/com/thomaskuenneth/counterdemo_compose/MainActivity.kt
@@ -4,15 +4,18 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
 import androidx.compose.Model
+import androidx.ui.core.Alignment.Companion.Center
 import androidx.ui.core.Alignment.Companion.CenterHorizontally
+import androidx.ui.core.Modifier
 import androidx.ui.core.setContent
 import androidx.ui.foundation.Box
 import androidx.ui.foundation.Text
-import androidx.ui.layout.Arrangement
-import androidx.ui.layout.Column
+import androidx.ui.layout.*
 import androidx.ui.material.Button
 import androidx.ui.material.MaterialTheme
+import androidx.ui.material.Typography
 import androidx.ui.text.style.TextAlign
+import androidx.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,38 +29,53 @@ class MainActivity : AppCompatActivity() {
 }
 
 @Model
-class CounterState(var counter: Int = 0)
+class CounterState(var counter: Int)
 
 @Composable
-fun Counter(state: CounterState) {
+fun CounterDemo() {
+    val state = CounterState(0)
     Column(
-        horizontalGravity = CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-
-            if (state.counter == 0) {
-                Text(
-                    text = "Noch nicht geklickt",
-                    textAlign = TextAlign.Center,
-                    style = MaterialTheme.typography.h3
-//                    paragraphStyle = ParagraphStyle(textAlign = TextAlign.Center),
-                )
-            } else {
-                Text(
-                    text = "${state.counter}",
-                    textAlign = TextAlign.Center,
-                    style = MaterialTheme.typography.h1
-                )
-            }
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        horizontalGravity = CenterHorizontally
+    )
+    {
+        Box(
+            gravity = Center,
+            padding = 16.dp,
+            modifier = Modifier.height(200.dp)
+        ) {
+            CountText(state)
         }
-        Button(
-            onClick = { state.counter++ }) {
-            Text("Klick")
-
+        CountButton(state)
     }
 }
 
 @Composable
-fun CounterDemo() {
-    Counter(state = CounterState())
+fun CountText(state: CounterState) {
+    val typography = Typography()
+    if (state.counter == 0) {
+        Text(
+            text = "Noch nicht geklickt",
+            softWrap = true,
+            textAlign = TextAlign.Center,
+            style = typography.h3
+        )
+    } else {
+        Text(
+            text = "${state.counter}",
+            textAlign = TextAlign.Center,
+            style = typography.h1
+        )
+    }
+}
+
+@Composable
+fun CountButton(state: CounterState) {
+    Button(
+        onClick = { state.counter++ }) {
+        Text(text = "Klick")
+    }
 }
