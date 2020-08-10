f5d2e46f5ea97df73d07d3b3f4c86a4a2327802b
Thomas Kuenneth <thomas.kuenneth@mathema.de>
2020-08-10 13:48:42 +0200

    Android project added

---
+++ CSharp/CounterDemoXamarin/CounterDemoXamarin.Droid/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.OS;
+using Xamarin.Forms.Platform.Android;
+
+namespace CounterDemoXamarin.Droid
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+
+            Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+    }
+}
