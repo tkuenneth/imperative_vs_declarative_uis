bcd5761f9a0a12dea50d24cbda303de0e510d074
Thomas Künneth <thomas@thomaskuenneth.eu>
2022-05-17 11:32:15 +0200

    Made code less sophisticated

--- SwiftUI/CounterDemoSwiftUI/CounterDemoSwiftUI/ContentView.swift
+++ SwiftUI/CounterDemoSwiftUI/CounterDemoSwiftUI/ContentView.swift
@@ -1,32 +1,27 @@
 import SwiftUI
 
 struct ContentView: View {
-
+    
     @State var counter = 0
-
+    
     var body: some View {
         VStack {
-            if (counter == 0) {
-                Text("Noch nicht geklickt")
-                    .font(Font.system(size: 14))
-                    .italic()
-                    .addFrame(withHeight: 100)
-            } else {
-                Text("\(counter)")
-                    .font(Font.system(size: 72))
-                    .bold()
-                    .addFrame(withHeight: 100)
+            VStack {
+                if (counter == 0) {
+                    Text("Noch nicht geklickt")
+                        .font(Font.system(size: 14))
+                        .italic()
+                } else {
+                    Text("\(counter)")
+                        .font(Font.system(size: 72))
+                        .bold()
+                }
             }
+            .frame(height: 100)
             Button("Klick") {
                 self.counter += 1
             }
         }
-    }
-}
-
-extension Text {
-    func addFrame(withHeight height: CGFloat) -> some View {
-        self.frame(height: height).fixedSize()
     }
 }
 
