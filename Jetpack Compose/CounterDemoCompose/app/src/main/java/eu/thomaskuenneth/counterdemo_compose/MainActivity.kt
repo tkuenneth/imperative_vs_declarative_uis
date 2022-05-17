b4d406d62ff73b819f70a0a28273715bce1a8410
Thomas Künneth <thomas@thomaskuenneth.eu>
2022-05-17 11:45:56 +0200

    Refactoring

--- Jetpack Compose/CounterDemoCompose/app/src/main/java/eu/thomaskuenneth/counterdemo_compose/MainActivity.kt
+++ Jetpack Compose/CounterDemoCompose/app/src/main/java/eu/thomaskuenneth/counterdemo_compose/MainActivity.kt
@@ -3,12 +3,23 @@ package eu.thomaskuenneth.counterdemo_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
@@ -16,74 +27,73 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import eu.thomaskuenneth.counterdemo_compose.R
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CounterDemo()
+            MaterialTheme {
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            title = {
+                                Text(text = stringResource(id = R.string.app_name))
+                            }
+                        )
+                    }
+                ) {
+                    CounterDemo()
+                }
+            }
         }
     }
 }
 
 @Composable
 fun CounterDemo() {
-    val state = remember { mutableStateOf(0) }
-    MaterialTheme {
-        Scaffold(
-            topBar = {
-                TopAppBar(
-                    title = {
-                        Text(text = stringResource(id = R.string.app_name))
-                    }
-                )
-            }
+    var state by remember { mutableStateOf(0) }
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        horizontalAlignment = CenterHorizontally
+    )
+    {
+        Box(
+            contentAlignment = Center,
+            modifier = Modifier.height(200.dp)
         ) {
-            Column(
-                verticalArrangement = Arrangement.Center,
-                modifier = Modifier
-                    .padding(16.dp)
-                    .fillMaxWidth()
-                    .fillMaxHeight(),
-                horizontalAlignment = CenterHorizontally
-            )
-            {
-                Box(
-                    contentAlignment = Center,
-                    modifier = Modifier.height(200.dp)
-                ) {
-                    CountText(state.value)
-                }
-                CountButton(state)
-            }
+            CountText(state)
         }
+        CountButton { state += 1 }
     }
 }
 
 @Composable
 fun CountText(value: Int) {
-    val typography = Typography()
     if (value == 0) {
         Text(
             text = "Noch nicht geklickt",
             softWrap = true,
             textAlign = TextAlign.Center,
-            style = typography.h3
+            style = MaterialTheme.typography.h3
         )
     } else {
         Text(
             text = "$value",
             textAlign = TextAlign.Center,
-            style = typography.h1
+            style = MaterialTheme.typography.h1
         )
     }
 }
 
 @Composable
-fun CountButton(state: MutableState<Int>) {
+fun CountButton(onClick: () -> Unit) {
     Button(
-        onClick = { state.value++ }) {
+        onClick = onClick
+    ) {
         Text(text = "Klick")
     }
 }
