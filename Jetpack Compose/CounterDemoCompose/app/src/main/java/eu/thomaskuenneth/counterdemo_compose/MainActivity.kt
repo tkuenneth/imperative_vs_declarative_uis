e86d1cd3c52f541e1da6e2144dde01728a99806c
Thomas Künneth <kuennetht@gmail.com>
2023-04-28 16:32:58 +0200

    Library and tools upgrades

--- Jetpack Compose/CounterDemoCompose/app/src/main/java/eu/thomaskuenneth/counterdemo_compose/MainActivity.kt
+++ Jetpack Compose/CounterDemoCompose/app/src/main/java/eu/thomaskuenneth/counterdemo_compose/MainActivity.kt
@@ -3,15 +3,24 @@ package eu.thomaskuenneth.counterdemo_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -78,21 +87,4 @@ fun CounterDemo() {
             Text(text = "Klick")
         }
     }
-}
-
-@Composable
-@Preview
-fun Preview() {
-    CounterDemo()
-}
-
-@Preview
-@Composable
-fun SimpleDemo() {
-    var color by remember { mutableStateOf(Color.Blue) }
-    Box(modifier = Modifier
-        .width(128.dp)
-        .height(64.dp)
-        .clickable { color = if (color == Color.Blue) Color.Red else Color.Blue }
-        .background(color))
 }
