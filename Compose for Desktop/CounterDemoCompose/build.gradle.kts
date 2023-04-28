2058d7e64b0f44f424c88bfed1e7e46a4e9d280c
Thomas Künneth <kuennetht@gmail.com>
2023-04-28 16:39:59 +0200

    Library and tools upgrades

--- Compose for Desktop/CounterDemoCompose/build.gradle.kts
+++ Compose for Desktop/CounterDemoCompose/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.7.10"
-    id("org.jetbrains.compose") version "1.2.1"
+    kotlin("jvm") version "1.8.0"
+    id("org.jetbrains.compose") version "1.4.0"
 }
 
 repositories {
